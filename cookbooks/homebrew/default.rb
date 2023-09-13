9918d16092d9d066f61edef5250b2ad8d3d724c5
basashi <basashi0414@gmail.com>
2023-09-13 19:09:27 +0900

    homebrewのインストール手順を追加

--- cookbooks/homebrew/default.rb
+++ cookbooks/homebrew/default.rb
@@ -1,3 +1,7 @@
+execute '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"' do
+  not_if 'which brew'
+end
+
 link File.join(ENV['HOME'], '.Brewfile') do
   to File.expand_path('../files/Brewfile', __FILE__)
   force true
