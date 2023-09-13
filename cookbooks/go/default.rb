63019450bb043532b8d9643e8f2f80e478d47ded
basashi <basashi0414@gmail.com>
2023-09-14 01:24:58 +0900

    一度変数に落とす

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -1,8 +1,10 @@
-[
+cmds = [
   'golang.org/x/tools/cmd/goimports',
   'honnef.co/go/tools/cmd/staticcheck',
   'github.com/Songmu/make2help/cmd/make2help',
-].each do |cmd|
+]
+
+cmds.each do |cmd|
   execute "go install #{cmd}@latest" do
     not_if "which #{cmd.split('/').last}"
   end
