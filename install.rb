63b944315c05f54888439827c51d340cb28c2e80
basashi <basashi0414@gmail.com>
2023-09-14 09:15:29 +0900

    includeのパス修正

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 MItamae::RecipeContext.class_eval do
   def include_cookbook(name)
     root_dir = File.expand_path('..', __FILE__)
-    include_recipe File.join(root_dir, 'cookbooks', name)
+    include_recipe File.join(root_dir, 'cookbooks', name, 'default')
   end
 end
 
