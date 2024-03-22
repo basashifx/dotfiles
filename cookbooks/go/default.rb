4f865440a342d9bfefd97b3af41ccc2d91dcf135
basashi <basashi0414@gmail.com>
2024-03-22 22:34:18 +0900

    gotestsum追加

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -2,6 +2,7 @@ cmds = [
   'golang.org/x/tools/cmd/goimports',
   'honnef.co/go/tools/cmd/staticcheck',
   'github.com/Songmu/make2help/cmd/make2help',
+  'gotest.tools/gotestsum',
 ]
 
 cmds.each do |cmd|
