27d57d6a4947d01765c8852bc9940c7b2d91d347
basashi <basashi0414@gmail.com>
2024-03-30 16:05:30 +0900

    gomockを追加

--- cookbooks/go/default.rb
+++ cookbooks/go/default.rb
@@ -3,6 +3,7 @@ cmds = [
   'honnef.co/go/tools/cmd/staticcheck',
   'github.com/Songmu/make2help/cmd/make2help',
   'gotest.tools/gotestsum',
+  'github.com/golang/mock/mockgen',
 ]
 
 cmds.each do |cmd|
